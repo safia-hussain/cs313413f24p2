4c7eac8914b99b296685aa7c91b0eedfa538417b
shussain77 <shussain22@luc.edu>
2024-09-25 02:26:20 -0500

    Project 2 Completion

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -1,208 +1,248 @@
 package cs271.lab.list;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class TestList {
 
-  private List<Integer> list;
+    private List<Integer> list;
 
-  @Before
-  public void setUp() throws Exception {
-    list = new ArrayList<Integer>();
-    // TODO also try with a LinkedList - does it make any difference?
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    list = null;
-  }
-
-  @Test
-  public void testSizeEmpty() {
-    assertTrue(list.isEmpty());
-    assertEquals(0, list.size());
-    try {
-      list.get(0);
-      fail("there should not be any items in the list");
-    } catch (Exception ex) {
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList<Integer>();
+        // TODO also try with a LinkedList - does it make any difference?
+        // list = new LinkedList<Integer>();
+        // just like testiterator.java, we can keep the ArrayList
     }
-  }
 
-  @Test
-  public void testSizeNonEmpty() {
-    // TODO fix the expected values in the assertions below
-    list.add(77);
-    assertEquals(true, list.isEmpty());
-    assertEquals(0, list.size());
-    assertEquals(0, list.get(0).intValue());
-  }
+    @After
+    public void tearDown() throws Exception {
+        list = null;
+    }
 
-  @Test
-  public void testContains() {
-    // TODO write assertions using
-    // list.contains(77)
-    // that hold before and after adding 77 to the list
-    fail("Not yet implemented"); // remove this line when done
-  }
+    @Test
+    public void testSizeEmpty() {
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+        try {
+            list.get(0);
+            fail("there should not be any items in the list");
+        } catch (Exception ex) {
+        }
+    }
 
-  @Test
-  public void testAddMultiple() {
-    list.add(77);
-    list.add(77);
-    list.add(77);
-    // TODO fix the expected values in the assertions below
-    assertEquals(0, list.size());
-    assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.get(1).intValue());
-    assertEquals(0, list.lastIndexOf(77));
-  }
+    @Test
+    public void testSizeNonEmpty() {
+        // TODO fix the expected values in the assertions below
+        list.add(77);
+        // false, size is not empty
+        assertEquals(false, list.isEmpty());
+        // only 1 element in the list, 77
+        assertEquals(1, list.size());
+        // 77 is the value of the first element as declared in line 45
+        assertEquals(77, list.get(0).intValue());
+    } // done
 
-  @Test
-  public void testAddMultiple2() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO fix the expected values in the assertions below
-    assertEquals(0, list.size());
-    assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.lastIndexOf(77));
-    assertEquals(0, list.get(2).intValue());
-    assertEquals(0, list.get(3).intValue());
-    assertEquals(List.of(33, 77, 44), list);
-  }
+    @Test
+    public void testContains() {
+        // TODO write assertions using
+        // list.contains(77)
+        // that hold before and after adding 77 to the list
+        assertFalse(list.contains(77)); // 77 has not yet been added to the list
+        list.add(77);
+        assertTrue(list.contains(77)); // 77 has now been added
+    } // done
 
-  @Test
-  public void testRemoveObject() {
-    list.add(3);
-    list.add(77);
-    list.add(4);
-    list.add(77);
-    list.add(5);
-    list.add(77);
-    list.add(6);
-    list.remove(5); // what does this method do?
-    // TODO fix the expected values in the assertions below
-    assertEquals(0, list.size());
-    assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.lastIndexOf(77));
-    assertEquals(0, list.get(2).intValue());
-    assertEquals(0, list.get(3).intValue());
-    list.remove(Integer.valueOf(5)); // what does this one do?
-    assertEquals(0, list.size());
-    assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.lastIndexOf(77));
-    assertEquals(0, list.get(2).intValue());
-    assertEquals(0, list.get(3).intValue());
-  }
+    @Test
+    public void testAddMultiple() {
+        list.add(77);
+        list.add(77);
+        list.add(77);
+        // TODO fix the expected values in the assertions below
+        // 3 elements have been added
+        assertEquals(3, list.size());
+        // first element starts at 0
+        assertEquals(0, list.indexOf(77));
+        // second element's value is 77
+        assertEquals(77, list.get(1).intValue());
+        // 3 elements = 0, 1, 2
+        assertEquals(2, list.lastIndexOf(77));
+    } // done
 
-  @Test
-  public void testContainsAll() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO using containsAll and List.of (see above),
-    // 1) assert that list contains all five different numbers added
-    // 2) assert that list does not contain all of 11, 22, and 33
-    fail("Not yet implemented"); // remove this line when done
-  }
+    @Test
+    public void testAddMultiple2() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO fix the expected values in the assertions below
+        // there are 7 elements
+        assertEquals(7, list.size());
+        // 77 is the second element at index 1
+        assertEquals(1, list.indexOf(77));
+        // the last 77 is the sixth element at index 5
+        assertEquals(5, list.lastIndexOf(77));
+        // index 2 holds 44
+        assertEquals(44, list.get(2).intValue());
+        // index 3 holds 77
+        assertEquals(77, list.get(3).intValue());
+        // correction on the list
+        assertEquals(List.of(33, 77, 44, 77, 55, 77, 66), list);
+    } // done
 
-  @Test
-  public void testAddAll() {
-    // TODO in a single statement using addAll and List.of,
-    // add items to the list to make the following assertions pass
-    // (without touching the assertions themselves)
-    assertEquals(7, list.size());
-    assertEquals(33, list.get(0).intValue());
-    assertEquals(77, list.get(1).intValue());
-    assertEquals(44, list.get(2).intValue());
-    assertEquals(77, list.get(3).intValue());
-    assertEquals(55, list.get(4).intValue());
-    assertEquals(77, list.get(5).intValue());
-    assertEquals(66, list.get(6).intValue());
-  }
+    @Test
+    public void testRemoveObject() {
+        list.add(3);
+        list.add(77);
+        list.add(4);
+        list.add(77);
+        list.add(5);
+        list.add(77);
+        list.add(6);
+        list.remove(5); // what does this method do?
+        // this method removes the integer at index 5
+        // TODO fix the expected values in the assertions below
+        // size is 6 after removing element in index 5
+        assertEquals(6, list.size());
+        // the first 77 appears at index 1
+        assertEquals(1, list.indexOf(77));
+        // after removing index 5, the last 77 is at index 3
+        assertEquals(3, list.lastIndexOf(77));
+        // value 4 is at index 2
+        assertEquals(4, list.get(2).intValue());
+        // value 77 is at index 3
+        assertEquals(77, list.get(3).intValue());
+        list.remove(Integer.valueOf(5)); // what does this one do?
+        // this method removes the element that holds the value of 5, at index 4
+        // after removing index 4 and 5, it is now size 5
+        assertEquals(5, list.size());
+        // first instance of 77 is still at index 1
+        assertEquals(1, list.indexOf(77));
+        // last instance after removing index 5 and 6 is 3
+        assertEquals(3, list.lastIndexOf(77));
+        // value 4 is still at index 2
+        assertEquals(4, list.get(2).intValue());
+        // value 77 is still at index 3
+        assertEquals(77, list.get(3).intValue());
+    } // done
 
-  @Test
-  public void testRemoveAll() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO in a single statement using removeAll and List.of,
-    // remove items from the list to make the following assertions pass
-    // (without touching the assertions themselves)
-    assertEquals(3, list.size());
-    assertEquals(List.of(77, 77, 77), list);
-  }
+    @Test
+    public void testContainsAll() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO using containsAll and List.of (see above),
+        // 1) assert that list contains all five different numbers added
+        // 2) assert that list does not contain all of 11, 22, and 33
+        // list has unique values, true
+        assertTrue(list.containsAll(List.of(33, 77, 44, 55, 66)));
+        // list has the following values, false
+        assertFalse(list.containsAll(List.of(11, 22, 33)));
 
-  @Test
-  public void testRetainAll() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO in a single statement using retainAll and List.of,
-    // remove items from the list to make the following assertions pass
-    // (without touching the assertions themselves)
-    assertEquals(3, list.size());
-    assertEquals(List.of(77, 77, 77), list);
-  }
+    } // done
 
-  @Test
-  public void testSet() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO use the set method to change specific elements in the list
-    // such that the following assertions pass
-    // (without touching the assertions themselves)
-    assertEquals(7, list.size());
-    assertEquals(33, list.get(0).intValue());
-    assertEquals(99, list.get(1).intValue());
-    assertEquals(44, list.get(2).intValue());
-    assertEquals(99, list.get(3).intValue());
-    assertEquals(55, list.get(4).intValue());
-    assertEquals(99, list.get(5).intValue());
-    assertEquals(66, list.get(6).intValue());
-  }
+    @Test
+    public void testAddAll() {
+        // TODO in a single statement using addAll and List.of,
+        // add items to the list to make the following assertions pass
+        // (without touching the assertions themselves)
+        list.addAll(List.of(33, 77, 44, 77, 55, 77, 66)); // adding all elements in one line in order
+        assertEquals(7, list.size());
+        assertEquals(33, list.get(0).intValue());
+        assertEquals(77, list.get(1).intValue());
+        assertEquals(44, list.get(2).intValue());
+        assertEquals(77, list.get(3).intValue());
+        assertEquals(55, list.get(4).intValue());
+        assertEquals(77, list.get(5).intValue());
+        assertEquals(66, list.get(6).intValue());
+    } // done
 
-  @Test
-  public void testSubList() {
-    list.add(33);
-    list.add(77);
-    list.add(44);
-    list.add(77);
-    list.add(55);
-    list.add(77);
-    list.add(66);
-    // TODO fix the arguments in the subList method so that the assertion
-    // passes
-    assertEquals(List.of(44, 77, 55), list.subList(0, 0));
-  }
+    @Test
+    public void testRemoveAll() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO in a single statement using removeAll and List.of,
+        // remove items from the list to make the following assertions pass
+        // (without touching the assertions themselves)
+        list.removeAll(List.of(33, 44, 55, 66)); // removing by referencing the values themselves
+        assertEquals(3, list.size());
+        assertEquals(List.of(77, 77, 77), list);
+    } // done
+
+    @Test
+    public void testRetainAll() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO in a single statement using retainAll and List.of,
+        // remove items from the list to make the following assertions pass
+        // (without touching the assertions themselves)
+        list.retainAll(List.of(77)); // only keep the values of 77
+        assertEquals(3, list.size());
+        assertEquals(List.of(77, 77, 77), list);
+    }
+
+    @Test
+    public void testSet() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO use the set method to change specific elements in the list
+        // such that the following assertions pass
+        // (without touching the assertions themselves)
+        // index 1, 3, 5 need to be set to value 99
+        list.set(1, 99);
+        list.set(3, 99);
+        list.set(5, 99);
+        assertEquals(7, list.size());
+        assertEquals(33, list.get(0).intValue());
+        assertEquals(99, list.get(1).intValue());
+        assertEquals(44, list.get(2).intValue());
+        assertEquals(99, list.get(3).intValue());
+        assertEquals(55, list.get(4).intValue());
+        assertEquals(99, list.get(5).intValue());
+        assertEquals(66, list.get(6).intValue());
+    } // done
+
+    @Test
+    public void testSubList() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        // TODO fix the arguments in the subList method so that the assertion
+        // passes
+        //  refers to the index of the start and end of the sublist
+        assertEquals(List.of(44, 77, 55), list.subList(2, 5));
+    }
 }
--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -3,72 +3,101 @@ package cs271.lab.list;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestPerformance {
 
-  // TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
-  // (choose in conjunction with REPS below up to an upper limit where the clock
-  // running time is in the tens of seconds)
-  // TODO (optional) refactor to DRY
-  // which of the two lists performs better as the size increases?
-  private final int SIZE = 10;
+    // TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
+    // (choose in conjunction with REPS below up to an upper limit where the clock
+    // running time is in the tens of seconds)
+    // this will declare the size of each test
+    private final int[] SIZES = {10, 100, 1000, 10000};
+    // TODO (optional) refactor to DRY
+    // which of the two lists performs better as the size increases?
+    // private final int SIZE = 10;
 
-  // TODO choose this value in such a way that you can observe an actual effect
-  // for increasing problem sizes
-  private final int REPS = 1000000;
+    // TODO choose this value in such a way that you can observe an actual effect
+    // for increasing problem sizes
+    private final int REPS = 1000000; // choose this one, this will perform better because it is larger
 
-  private List<Integer> arrayList;
+    private List<Integer> arrayList;
 
-  private List<Integer> linkedList;
+    private List<Integer> linkedList;
 
-  @Before
-  public void setUp() throws Exception {
-    arrayList = new ArrayList<Integer>(SIZE);
-    linkedList = new LinkedList<Integer>();
-    for (var i = 0; i < SIZE; i++) {
-      arrayList.add(i);
-      linkedList.add(i);
+    @Before
+    public void setUp() throws Exception {
+        // calculate length of array - 1, creates them all
+        arrayList = new ArrayList<Integer>(SIZES[SIZES.length - 1]);
+        linkedList = new LinkedList<Integer>();
+        for (var i = 0; i < SIZES[SIZES.length - 1]; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
     }
-  }
 
-  @After
-  public void tearDown() throws Exception {
-    arrayList = null;
-    linkedList = null;
-  }
-
-  @Test
-  public void testLinkedListAddRemove() {
-    for (var r = 0; r < REPS; r++) {
-      linkedList.add(0, 77);
-      linkedList.remove(0);
+    @After
+    public void tearDown() throws Exception {
+        // don't need to change
+        arrayList = null;
+        linkedList = null;
     }
-  }
 
-  @Test
-  public void testArrayListAddRemove() {
-    for (var r = 0; r < REPS; r++) {
-      arrayList.add(0, 77);
-      arrayList.remove(0);
+    @Test
+    public void testLinkedListAddRemove() {
+        for (int size : SIZES) {
+            long startTime = System.nanoTime(); // starts timer
+            linkedList.clear(); // clears linkedList
+            for (var r = 0; r < REPS; r++) {
+                linkedList.add(0, 77);
+                linkedList.remove(0);
+            }
+            long endTime = System.nanoTime(); // end timer
+            // subtracting start time to end time to get total seconds
+            // dividing by 1e6 for proper time display
+            System.out.println("LinkedList Add/Remove with SIZE " + size + ": " + (endTime - startTime) / 1e6 + " ms");
+        }
     }
-  }
 
-  @Test
-  public void testLinkedListAccess() {
-    var sum = 0L;
-    for (var r = 0; r < REPS; r++) {
-      sum += linkedList.get(r % SIZE);
+    @Test
+    public void testArrayListAddRemove() {
+        for (int size : SIZES) {
+            long startTime = System.nanoTime(); // start timer
+            arrayList.clear(); // clear list
+            for (var r = 0; r < REPS; r++) {
+                arrayList.add(0, 77);
+                arrayList.remove(0);
+            }
+            long endTime = System.nanoTime(); // end timer
+            System.out.println("ArrayList Add/Remove with SIZE " + size + ": " + (endTime - startTime) / 1e6 + " ms");
+        }
     }
-  }
 
-  @Test
-  public void testArrayListAccess() {
-    var sum = 0L;
-    for (var r = 0; r < REPS; r++) {
-      sum += arrayList.get(r % SIZE);
+    @Test
+    public void testLinkedListAccess() {
+        for (int size : SIZES) {
+            long startTime = System.nanoTime(); // start timer
+            var sum = 0L;
+            for (var r = 0; r < REPS; r++) {
+                sum += linkedList.get(r % size);
+            }
+            long endTime = System.nanoTime(); // end timer
+            System.out.println("LinkedList Access with SIZE " + size + ": " + (endTime - startTime) / 1e6 + " ms");
+        }
     }
-  }
+
+    @Test
+    public void testArrayListAccess() {
+        for (int size : SIZES) {
+            long startTime = System.nanoTime(); // start timer
+            var sum = 0L;
+            for (var r = 0; r < REPS; r++) {
+                sum += arrayList.get(r % size);
+            }
+            long endTime = System.nanoTime(); // end timer
+            System.out.println("ArrayList Access with SIZE " + size + ": " + (endTime - startTime) / 1e6 + " ms");
+        }
+    }
 }
